8865e6327d6be1e159e95c27b050690210898bb8
Apoorva <apoorvas745sharma@gmail.com>
2025-02-11 00:43:46 +0530

    main menu basic structre

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Video;
+
+public class Button : MonoBehaviour
+{
+    public VideoPlayer videoPlayer; 
+    public GameObject currentcanvas;
+    public GameObject VideoCanvas;
+    public float delaytime = 2f;
+
+    public void Start()
+    {
+        VideoCanvas.SetActive(false);
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Button Clicked");
+        currentcanvas.SetActive(false); 
+        VideoCanvas.SetActive(true);
+        videoPlayer.Play();
+        StartCoroutine(LoadNextScene());
+    }
+     IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(delaytime); 
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+
+}
