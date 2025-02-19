ed8cf565d1de48271276ed826202e6568087ad83
Apoorva <apoorvas745sharma@gmail.com>
2025-02-19 13:47:01 +0530

    CheckPoint bna dla re

---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    private Vector2 respawnPoint; 
+    // public GameObject deathCanvas;
+
+    void Start()
+    {
+        respawnPoint = transform.position; 
+        // if (deathCanvas != null)
+        // {
+        //     deathCanvas.SetActive(false);
+        // }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Checkpoint")) 
+        {
+            respawnPoint = other.transform.position; 
+            Debug.Log("Checkpoint updated to: " + respawnPoint);
+        }
+        else if (other.CompareTag("EnemyBullet")) 
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        
+       
+        
+            Respawn(); 
+        
+    }
+
+    public void Respawn()
+    {
+        // Time.timeScale = 1f; // Resume game
+        transform.position = respawnPoint; // Move player to the last checkpoint
+        // if (deathCanvas != null)
+        // {
+        //     deathCanvas.SetActive(false); // Hide death screen
+        // }
+    }
+}
